03376329fe2a4d3ff2ed352395a17de9f94f7b98
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-01-02 08:58:33 +0500

    step17

--- step15/src/pages/index.js
+++ step15/src/pages/index.js
@@ -1,5 +1,21 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
 
-export default function Home() {
-  return <div>Hello world from Hassan Ali Khan</div>
+export default () => {
+  const [mydata, setData] = useState("Default Hello");
+
+  useEffect(() => {
+    console.log("useEffect Called");
+    fetch(`/.netlify/functions/hello?name=from Serverless Function`)
+      .then(response => response.json())
+      .then(data => {
+        setData(data);
+        console.log("Data: " + JSON.stringify(data));
+        
+      });
+  }, []);
+
+  return <div>
+  <div>Hello world with Gatsby from Netlify!</div>
+  <div>{mydata.message}</div>
+  </div>
 }
