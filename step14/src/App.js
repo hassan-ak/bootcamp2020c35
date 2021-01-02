51b7e870e7877a2eb5360179df6f1b39ae5acaea
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-01-02 07:29:24 +0500

    Step14

---
+++ step14/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App" id="file">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello World!
+        </p>
+        <a
+          className="App-link"
+          href="#file"
+          rel="noopener noreferrer"
+        >
+          by: Hassan ali Khan
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
