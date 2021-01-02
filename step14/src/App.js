ca6371b7cea81e89c16e5e4098fe5f9d648965a2
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-01-02 08:45:42 +0500

    step16

--- step14/src/App.js
+++ step14/src/App.js
@@ -1,13 +1,22 @@
 import logo from './logo.svg';
+import React , {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch(`/.netlify/functions/hello?name=from Serverless Function`)
+      .then(response => response.json())
+      .then(data => setData(data));
+  }, []);
+
   return (
     <div className="App" id="file">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World!
+          {data.message}
         </p>
         <a
           className="App-link"
