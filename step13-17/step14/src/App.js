3880374c2b542576944f76680ae32d6ca282e480
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-01-26 15:41:31 +0500

    Step16 (react netlify functions helloworld)
    1:- Update netlify.toml
    2:- "netlify functions:create hello"
    3:- update App.js

--- step13-17/step14/src/App.js
+++ step13-17/step14/src/App.js
@@ -1,13 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import React , {useState, useEffect} from 'react';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch(`/.netlify/functions/hello?name=from Serverless Function`)
+      .then(response => response.json())
+      .then(data => setData(data));
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello world!
+          {data.message}
         </p>
         <a
           className="App-link"
